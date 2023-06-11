39514eb7627f40f326a5d9223a3f6ef5b9fb0151
LAPTOP-LENOG4CV\binnie <psubbb@gmail.com>
2023-06-11 22:21:36 +0900

    로그인 페이지 제작 완료_HTML, CSS

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,52 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e1cafcb9eeed0c95c6f45f01a5755bb7"></script>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	<script src="./js/jquery.min.js"></script>
+</head>
+<body>
+	<div id="map" style="width: 100%; height: 100vh;">
+		<section class="search_box">
+			<div class="search_wrap">
+				<div class="search">
+					<div class="search_menu">
+						<img src="./images/menu.svg">
+					</div>
+					<div class="search_input">
+						<input type="text" name="" placeholder="제주대학교 아라캠퍼스">
+					</div>
+					<div class="search_icon">
+						<img src="./images/search.svg">
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="main_bottom">
+			<div class="bottom_wrap">
+				<div class="bottom_title">
+					<p>경로설정</p>
+				</div>
+				<div class="bottom_input">
+					<div class="bottom_box">
+						<ul>
+							<li><input type="text" name="" placeholder="출발지" value="제주대학교 공과대학 4호관"></li>
+							<li><input type="text" name="" placeholder="목적지" id="goal"></li>
+						</ul>
+					</div>
+					<div class="bottom_icon">
+						<p><img src="./images/change.svg"></p>
+					</div>
+				</div>
+				<div class="bottom_btn">
+					<a href="javascript:void(0);" onclick="SetOnClick();">목적지 설정</a>
+				</div>
+			</div>
+		</section>
+	</div>
+</body>
+<script type="text/javascript" src="./js/map.js"></script>
+</html>
--- htdocs/login/login.php
+++ htdocs/login/login.php
@@ -1,99 +1,61 @@
-<?php
-    session_start();
-    if(isset($_SESSION["user_id"])){
-    header("Location:../index.php");
-    }
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>어쩌다 제주</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<title>Odiga</title>
+	<link rel="stylesheet" type="text/css" href="../css/login.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-   <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+	<script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+	<script src="../js/kakaoLogin.js"></script>
 </head>
 <body>
 	<section class="login_section">
-		<form class="login_wrap" action="login_process.php" method="POST">
+		<div class="login_wrap">
 			<div class="logo_area">
-				<p class="logo"><img src="../images/logo.svg"></p>
+				<p><img src="../images/logo.svg"></p>
 			</div>
-			<div class="login_area">
-				<div class="login_title">
-					<p>로그인</p>
-				</div>
+			<form class="login_area" method="POST" action="./login_process.php">
 				<ul>
-					<li><input type="text" name="id" placeholder="아이디" class="login_input"></li>
-					<li><input type="password" name="pw" placeholder="비밀번호" class="login_input"></li>
 					<li>
-						<input type="submit" name="" value="로그인" class="login_btn">
+						<p>아이디</p>
+						<input type="text" name="id" placeholder="아이디">
 					</li>
 					<li>
-						<p><a href="#">회원가입</a></p>
-						<p><a href="#">아이디 찾기</a> | <a href="#">비밀번호</a></p>
+						<p>비밀번호</p>
+						<input type="password" name="pw" placeholder="비밀번호">
 					</li>
 				</ul>
-				<i><span></span><p>SNS 간편 로그인</p></i>
-				<div class="kakao_login" onclick="kakaoLogin();"><a href="javascript:void(0)"><div><img src="../images/kakao.svg"><p>카카오 로그인</p></div></a></div>
-			</div>
-		</form>
+				<div class="login_keep">
+					<div class="keep">
+						<input type="checkbox" name="" id="myCheck">
+						<label for="myCheck"></label>
+						<p>로그인 상태 유지</p>
+					</div>
+					<div class="forgot_pass">
+						<a href="#">비밀번호 찾기</a>
+					</div>
+				</div>
+				<div class="login_btn">
+					<p><input type="submit" value="로그인"></p>
+				</div>
+				<div class="login_bottom">
+					<div class="login_other">
+						<i>&nbsp;</i>
+						<p>간편 회원가입</p>
+						<i>&nbsp;</i>
+					</div>
+					<div class="kakao_login" onclick="kakaoLogin();">
+						<a href="javascript:void(0)">
+							<p><img src="../images/kakao.svg"></p>
+							<p>카카오 로그인</p>
+						</a>
+					</div>
+				</div>
+				<div class="login_join">
+					<a href="./join.php">회원이 아니신가요? &nbsp;<span>회원가입</span></a>
+				</div>
+			</form>
+		</div>
 	</section>
 </body>
-<!-- 카카오 스크립트 -->
-<script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-<script>
-Kakao.init('a1bfedfd24ea1a4bf1ad63d174fae802'); //발급받은 키 중 javascript키를 사용해준다.
-console.log(Kakao.isInitialized()); // sdk초기화여부판단
-let responseData;
-//카카오로그인
-function kakaoLogin() {
-    Kakao.Auth.login({
-      success: function (response) {
-        Kakao.API.request({
-          url: '/v2/user/me',
-          success: function (response) {
-             responseData = response;
-             saveToDatabase(responseData);
-             location.href = "../index.php";
-          },
-          fail: function (error) {
-            console.log(error)
-          },
-        })
-      },
-      fail: function (error) {
-        console.log(error)
-      },
-    })
-  }
-//카카오로그아웃  
-function kakaoLogout() {
-    if (Kakao.Auth.getAccessToken()) {
-      Kakao.API.request({
-        url: '/v1/user/unlink',
-        success: function (response) {
-           console.log(response)
-        },
-        fail: function (error) {
-          console.log(error)
-        },
-      })
-      Kakao.Auth.setAccessToken(undefined)
-    }
-  }  
-
-function saveToDatabase(responseData) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "saveToDatabase.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    let data = JSON.stringify(responseData);
-    xhr.send(data);
-}
-</script>
 </html>
